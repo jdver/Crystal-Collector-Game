f56704f25f9c88ad9b07c3ed316375986c78c0d0
JD Vera <jdvera1@gmail.com>
2019-03-25 19:15:42 -0700

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+/*var targetNumbers = [29, 39, 52, 68, 80, 95];
+var numberOptions = [1, 7, 10, 6];
+var wins = 0;
+var losses = 0;*/
+
+$(document).ready(function(){
+    $("p").click(function(){
+      $(this).hide();
+    });
+  });
+
+
+
+
+/*document.getElementById("crystal1").onclick = function() {myFunction()};
+function myFunction() {
+    document.getElementById("crystal1").innerHTML = "YOU CLICKED ME!";
+  }
+
+
+
+
+//<p>Math.floor(Math.random() * 100)) returns a random integer between 0 and 99 
+//(both included):</p>
+
+//<p id="demo"></p>
+
+
+
+//function getRndInteger(min, max) {
+//  return Math.floor(Math.random("#crystal") * (max - min + 1) ) + min;
+//}
