961886e66cb24c7ffb8d8670455e250bb57b0a64
JD Vera <jdvera1@gmail.com>
2019-06-27 01:10:52 -0700

    refactored javascript code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,102 +1,99 @@
-// First tried creating an array with variables but that did not work
-// var targetNumbers = [29, 39, 52, 68, 80, 95];
-// var numberOptions = [1, 7, 10, 6];
-// This function will allow our jQuery to work/dispaly
+// Refactored code
 
-$(function () {
-  // Create the jQuery that will display on our HTML, the score displays player's score count, the targetNumber will display target score
-  var targetScore = Math.floor(Math.random() * 102 + 19)
-  $('#target').text(targetScore)
-
-  var wins = 0
-  $('#wins').text(wins)
-
-  var losses = 0
-  $('#losses').text(losses)
-
-  var score = 0
-  $('#score').text('Your total score is: ' + score)
-
-  // create individual values for each crystal
-
-  var crystal1 = Math.floor(Math.random() * 12) + 1
-  var crystal2 = Math.floor(Math.random() * 12) + 1
-  var crystal3 = Math.floor(Math.random() * 12) + 1
-  var crystal4 = Math.floor(Math.random() * 12) + 1
-
-  // Create a function to reset our target score and player score to 0 when game is over
-  function ariseGame () {
-    // reset the userScore & update display
-    score = 0
-    $('#score').text('Your total score is: ' + score)
-
-    // generate a new targetScore & update display
-    targetScore = Math.floor(Math.random() * 102) + 19
-    $('#target').text(targetScore)
-
-    // generate new crystal values
-    crystal1 = Math.floor(Math.random() * 12) + 1
-    crystal2 = Math.floor(Math.random() * 12) + 1
-    crystal3 = Math.floor(Math.random() * 12) + 1
-    crystal4 = Math.floor(Math.random() * 12) + 1
+// Create crystal object/variable
+var crystal = {
+  crystal1:
+  {
+    name: 'Crystal1',
+    value: 0
+  },
+  crystal2:
+  {
+    name: 'Crystal2',
+    value: 0
+  },
+  crystal3:
+  {
+    name: 'Crystal3',
+    value: 0
+  },
+  crystal4:
+  {
+    name: 'Crystal4',
+    value: 0
   }
-  $('#crystal1').on('click', function () {
-    score = score + crystal1
-    $('#score').text('Your total score is: ' + score)
+}
+// Global variables
+var score = 0
+var targetScore = 0
+var wins = 0
+var losses = 0
 
-    if (score === targetScore) {
-      wins++
-      $('#wins').text(wins)
-      ariseGame()
-    } else if (score > targetScore) {
-      losses++
-      $('#losses').text(losses)
-      ariseGame()
-    }
-  })
+// Initialize game
+var getRandom = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
-  $('#crystal2').on('click', function () {
-    score = score + crystal2
-    $('#score').text('Your total score is: ' + score)
+var startGame = function () {
+  // Reset game
+  score = 0
 
-    if (score === targetScore) {
-      wins++
-      $('#wins').text(wins)
-      ariseGame()
-    } else if (score > targetScore) {
-      losses++
-      $('#losses').text(losses)
-      ariseGame()
-    }
-  })
+  targetScore = getRandom(19, 120)
 
-  $('#crystal3').on('click', function () {
-    score = score + crystal3
-    $('#score').text('Your total score is: ' + score)
+  crystal.crystal1.value = getRandom(1, 12)
+  crystal.crystal2.value = getRandom(1, 12)
+  crystal.crystal3.value = getRandom(1, 12)
+  crystal.crystal4.value = getRandom(1, 12)
 
-    if (score === targetScore) {
-      wins++
-      $('#wins').text(wins)
-      ariseGame()
-    } else if (score > targetScore) {
-      losses++
-      $('#losses').text(losses)
-      ariseGame()
-    }
-  })
+  console.log('Target score: ' + targetScore)
+  /* eslint-disable, no-undef */
+  $('#score').html(`Your total score is: ${score}`)
+  $('#target').html(targetScore)
+}
 
-  $('#crystal4').on('click', function () {
-    score = score + crystal4
-    $('#score').text('Your total score is: ' + score)
+var addValues = function (crystal) {
+  score = score + crystal.value
 
-    if (score === targetScore) {
-      wins++
-      $('#wins').text(wins)
-      ariseGame()
-    } else if (score > targetScore) {
-      losses++
-      $('#losses').text(losses)
-      ariseGame()
-    }
-  })
+  $('#score').html(score)
+  console.log(score)
+
+  winOrloss()
+}
+
+var winOrloss = function () {
+  if (score > targetScore) {
+    alert('Sorry, better luck next time!')
+    losses++
+    $('#losses').html(losses)
+
+    // restart game
+    startGame()
+  } else if (score === targetScore) {
+    alert('Congrats, you win!')
+    wins++
+    $('#wins').html(wins)
+
+    // restart game
+    startGame()
+  }
+}
+
+startGame()
+
+// On click functions
+$('#crystal1').on('click', function () {
+  addValues(crystal.crystal1)
 })
+
+$('#crystal2').on('click', function () {
+  addValues(crystal.crystal2)
+})
+
+$('#crystal3').on('click', function () {
+  addValues(crystal.crystal3)
+})
+
+$('#crystal4').on('click', function () {
+  addValues(crystal.crystal4)
+})
+/* eslint-enable no-undef */
